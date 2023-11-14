8cf4f778004e5da96ee40910baf20132638e3dff
Tiphaine Ollivier <tiphaine.ollivier1@gmail.com>
2023-11-14 15:12:34 +0100

    first trial at models

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,43 @@
 class TasksController < ApplicationController
 
-  def all
+  def index
+    @tasks= Task.all
+  end
+
+  def display
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
   end
   
+  def create
+    @task = Task.new(task_param)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_param)
+    redirect_to tasks_path
+  end
+
+  def delete
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path,  status: :see_other
+  end
+
+  private 
+
+  def task_param
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
